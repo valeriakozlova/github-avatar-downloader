7c90071b9c5be54e3bd229a0ce0e23f10ef31614
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-29 23:32:05 +0000

    Commit Step Seven

--- download_avatars.js
+++ download_avatars.js
@@ -17,5 +17,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  var listOfObejects = JSON.parse(result);
+  var listOfAvatars = [];
+  for (var i = 0; i < listOfObejects.length; i++) {
+    listOfAvatars.push(listOfObejects[i]["avatar_url"])
+  }
+  console.log(listOfAvatars);
 });
