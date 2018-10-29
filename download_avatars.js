d774657591993336fadb6ada2238091d4863eeb5
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-29 23:47:13 +0000

    Commit Step Eight

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = require('./secrets');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -24,3 +25,19 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   }
   console.log(listOfAvatars);
 });
+
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+           console.log('Response: ', response.statusCode,);
+         })
+         .pipe(fs.createWriteStream(filePath));
+
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
