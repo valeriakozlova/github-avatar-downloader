d32d6a3912ad581c7b7e83e829048e6f2890ed62
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-30 00:00:04 +0000

    Final Commit

--- download_avatars.js
+++ download_avatars.js
@@ -2,31 +2,6 @@ var request = require('request');
 var token = require('./secrets');
 var fs = require('fs');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': `token ${token.GITHUB_TOKEN}`
-    }
-  };
-
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
-}
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  var listOfObejects = JSON.parse(result);
-  var listOfAvatars = [];
-  for (var i = 0; i < listOfObejects.length; i++) {
-    listOfAvatars.push(listOfObejects[i]["avatar_url"])
-  }
-  console.log(listOfAvatars);
-});
-
-
 function downloadImageByURL(url, filePath) {
 
   request.get(url)
@@ -37,7 +12,28 @@ function downloadImageByURL(url, filePath) {
            console.log('Response: ', response.statusCode,);
          })
          .pipe(fs.createWriteStream(filePath));
-
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${token.GITHUB_TOKEN}`
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  var listOfObejects = JSON.parse(result);
+  for (var i = 0; i < listOfObejects.length; i++) {
+    downloadImageByURL(listOfObejects[i]["avatar_url"],`avatars/${listOfObejects[i]["login"]}.jpg`)
+  }
+});
+
+
+
